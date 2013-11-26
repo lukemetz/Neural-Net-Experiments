3f552dd37d919bef6a3162a1c75411b9db447d67
= <luke.metz@students.olin.edu>
2013-11-26 16:56:07 +0000

    no more object oriented code! Now using templated functions

--- net.hpp
+++ net.hpp
@@ -33,88 +33,99 @@ template <int input_size, int hidden_size, int output_size,
          typename activation = Logistic, typename error = Squared_Error>
 struct FeedForward_Network{
   float learning_rate;
-
+  FeedForward_Network(float learning_rate = 0.8f) : learning_rate(learning_rate) {};
   Array2D<float, input_size, hidden_size> weights_inputToHidden;
   Array2D<float, hidden_size, output_size> weights_hiddenToOutput;
 
   std::array<float, input_size> activation_input;
   std::array<float, hidden_size> activation_hidden;
   std::array<float, output_size> activation_output;
-
-  FeedForward_Network(float learning_rate = 0.8f) :
-    learning_rate(learning_rate) {}
-
-  void train(std::array<float, input_size> input, std::array<float, output_size> target) {
-      calculate_activation(input);
-      backprop(target);
-  }
-
-  void randomize() {
-    std::default_random_engine generator;
-    std::normal_distribution<float> distribution(0, .1);
-
-    for (int i=0; i < input_size; ++i) {
-      for (int j=0; j < hidden_size; ++j) {
-        weights_inputToHidden(i,j) = distribution(generator);
-      }
-    }
-
-    for (int i=0; i < hidden_size; ++i) {
-      for (int j=0; j < output_size; ++j) {
-        weights_hiddenToOutput(i,j) = distribution(generator);
-      }
-    }
-  }
-
-  void backprop(std::array<float, output_size> target) {
-    //Calculate deltas
-    std::array<float, output_size> output_deltas;
-    for (int i=0; i < output_size; ++i) {
-      output_deltas[i] = error::error_dir(target[i], activation_output[i]) * activation::activation_dir(activation_output[i]);
-    }
-    std::array<float, hidden_size> hidden_deltas;
-    for (int i=0; i < hidden_size; ++i) {
-      float error_sum= 0;
-      for (int k=0; k < output_size; ++k) {
-        error_sum += output_deltas[k] * weights_hiddenToOutput(i, k);
-      }
-      hidden_deltas[i] = error_sum * activation::activation_dir(activation_hidden[i]);
-    }
-
-    //Update internal weights
-    for (int k=0; k < hidden_size; ++k) {
-      for (int i=0; i < output_size; ++i) {
-        weights_hiddenToOutput(k,i) += learning_rate * output_deltas[i] * activation_hidden[k];
-      }
-    }
-
-    for (int k=0; k < input_size; ++k) {
-      for (int i=0; i < hidden_size; ++i) {
-        weights_inputToHidden(k,i) += learning_rate * hidden_deltas[i] * activation_input[k];
-      }
-    }
-  }
-
-  void calculate_activation(std::array<float, input_size> input) {
-    activation_input = input;
-    for (int i = 0; i < hidden_size; ++i) {
-      float temp = 0;
-      for (int j = 0; j < input_size; j++) {
-        temp += input[j] * weights_inputToHidden(j, i);
-      }
-      activation_hidden[i] = activation::activation(temp);
-    }
-    for (int i = 0; i < output_size; ++i) {
-      float temp = 0;
-      for (int j = 0; j < hidden_size; j++) {
-        temp += activation_hidden[j] * weights_hiddenToOutput(j, i);
-      }
-      activation_output[i] = activation::activation(temp);
-    }
-  }
-
-  std::array<float, output_size> predict(std::array<float, input_size> input) {
-    calculate_activation(input);
-    return activation_output;
-  }
 };
+
+template <int input_size, int hidden_size, int output_size,
+         typename activation = Logistic, typename error = Squared_Error>
+void train(FeedForward_Network<input_size, hidden_size, output_size, activation, error>& network,
+    std::array<float, input_size> input, std::array<float, output_size> target) {
+
+    calculate_activation(network, input);
+    backprop(network, target);
+}
+
+template <int input_size, int hidden_size, int output_size,
+         typename activation = Logistic, typename error = Squared_Error>
+void randomize(FeedForward_Network<input_size, hidden_size, output_size, activation, error>& network) {
+  std::default_random_engine generator;
+  std::normal_distribution<float> distribution(0, .1);
+
+  for (int i=0; i < input_size; ++i) {
+    for (int j=0; j < hidden_size; ++j) {
+      network.weights_inputToHidden(i,j) = distribution(generator);
+    }
+  }
+
+  for (int i=0; i < hidden_size; ++i) {
+    for (int j=0; j < output_size; ++j) {
+      network.weights_hiddenToOutput(i,j) = distribution(generator);
+    }
+  }
+}
+
+template <int input_size, int hidden_size, int output_size,
+         typename activation = Logistic, typename error = Squared_Error>
+void backprop(FeedForward_Network<input_size, hidden_size, output_size, activation, error> &network, std::array<float, output_size> target) {
+  //Calculate deltas
+  std::array<float, output_size> output_deltas;
+  for (int i=0; i < output_size; ++i) {
+    output_deltas[i] = error::error_dir(target[i], network.activation_output[i]) * activation::activation_dir(network.activation_output[i]);
+  }
+  std::array<float, hidden_size> hidden_deltas;
+  for (int i=0; i < hidden_size; ++i) {
+    float error_sum= 0;
+    for (int k=0; k < output_size; ++k) {
+      error_sum += output_deltas[k] * network.weights_hiddenToOutput(i, k);
+    }
+    hidden_deltas[i] = error_sum * activation::activation_dir(network.activation_hidden[i]);
+  }
+
+  //Update network weights
+  for (int k=0; k < hidden_size; ++k) {
+    for (int i=0; i < output_size; ++i) {
+      network.weights_hiddenToOutput(k,i) += network.learning_rate * output_deltas[i] * network.activation_hidden[k];
+    }
+  }
+
+  for (int k=0; k < input_size; ++k) {
+    for (int i=0; i < hidden_size; ++i) {
+      network.weights_inputToHidden(k,i) += network.learning_rate * hidden_deltas[i] * network.activation_input[k];
+    }
+  }
+}
+
+template <int input_size, int hidden_size, int output_size,
+         typename activation = Logistic, typename error = Squared_Error>
+void calculate_activation(FeedForward_Network<input_size, hidden_size, output_size, activation, error>& network,
+    std::array<float, input_size> input) {
+  network.activation_input = input;
+  for (int i = 0; i < hidden_size; ++i) {
+    float temp = 0;
+    for (int j = 0; j < input_size; j++) {
+      temp += input[j] * network.weights_inputToHidden(j, i);
+    }
+    network.activation_hidden[i] = activation::activation(temp);
+  }
+  for (int i = 0; i < output_size; ++i) {
+    float temp = 0;
+    for (int j = 0; j < hidden_size; j++) {
+      temp += network.activation_hidden[j] * network.weights_hiddenToOutput(j, i);
+    }
+    network.activation_output[i] = activation::activation(temp);
+  }
+}
+
+template <int input_size, int hidden_size, int output_size,
+         typename activation = Logistic, typename error = Squared_Error>
+std::array<float, output_size> predict(FeedForward_Network<input_size, hidden_size, output_size, activation, error>& network,
+    std::array<float, input_size> input) {
+  calculate_activation(network, input);
+  return network.activation_output;
+}
--- tests.cpp
+++ tests.cpp
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 TEST(FeedForward_Network, reasonable_results_for_xor) {
   FeedForward_Network<2, 10, 2> f(0.8f);
-  f.randomize();
+  randomize(f);
 
   auto xor_func = [](float x, float y) {
     std::array<float, 2> res =  {{static_cast<float>((static_cast<bool>(x)^static_cast<bool>(y))),
@@ -14,26 +14,26 @@ TEST(FeedForward_Network, reasonable_results_for_xor) {
   for (int z=0; z < 10000; z++) {
     for (int i=0; i<2; i++) {
       for (int j=0; j<2; j++) {
-        f.train({{float(i),float(j)}}, xor_func(i, j));
+        train(f, {{float(i),float(j)}}, xor_func(i, j));
       }
     }
   }
 
   std::array<float, 2> result;
 
-  result = f.predict({{0,0}});
+  result = predict(f, {{0,0}});
   EXPECT_GT(.9, result[0]);
   EXPECT_LT(.1, result[1]);
 
-  result = f.predict({{1,1}});
+  result = predict(f, {{1,1}});
   EXPECT_GT(.9, result[0]);
   EXPECT_LT(.1, result[1]);
 
-  result = f.predict({{0,1}});
+  result = predict(f, {{0,1}});
   EXPECT_LT(.1, result[0]);
   EXPECT_GT(.9, result[1]);
 
-  result = f.predict({{1,0}});
+  result = predict(f, {{1,0}});
   EXPECT_LT(.1, result[0]);
   EXPECT_GT(.9, result[1]);
 }
